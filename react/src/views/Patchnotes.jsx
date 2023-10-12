41d75d4713a838a57d63b38ff71cc644875824f0
Robin C. <robin.combeau@septeo.com>
2023-10-12 14:21:17 +0200

    feat patchnotes goal

--- react/src/views/Patchnotes.jsx
+++ react/src/views/Patchnotes.jsx
@@ -11,9 +11,22 @@ export default function Patchnotes() {
         <PageTitle text="Patchnotes" />
 
         <div className="text-bubble">
-          <h3>0.1</h3>
-          <p className="text-align-left">- patch content #1</p>
-          <p className="text-align-left">- patch content #2</p>
+          <h3 className="text-align-left">Release (date)</h3>
+        </div>
+
+        <div className="text-bubble">
+          <h3 className="text-align-left">0.2 - Stats (date)</h3>
+          <p className="text-align-left">- Attempts and success are now stored in database.</p>
+          <p className="text-align-left">- Various stats are now displayed in the app.</p>
+          <p className="text-align-left">- Only gamemodes the player didn't win today are on the list of suggested gamemodes.</p>
+        </div>
+
+        <div className="text-bubble">
+          <h3 className="text-align-left">0.1 - First version (date)</h3>
+          <p className="text-align-left">- Gamemodes : Standard, Wild, Classic, Twist.</p>
+          <p className="text-align-left">- Website works for all devices.</p>
+          <p className="text-align-left">- A new card is selected every day at XXam GMT+2.</p>
+          <p className="text-align-left">- Card bank is also updated once a day.</p>
         </div>
 
         <Button label="Go back" className="medium-button medium-button-margin-top scale" to="/" />
