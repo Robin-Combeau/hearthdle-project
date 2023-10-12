55965c67805d1ec6da3269b4db072eecf707bf8f
Robin C. <robin.combeau@septeo.com>
2023-10-12 11:58:19 +0200

    feat smaller card image

--- react/src/views/components/CardImage.jsx
+++ react/src/views/components/CardImage.jsx
@@ -4,9 +4,13 @@ export default function CardImage({ imageUrl, altText, imageLoaded = true }) {
     return (
         <div>
             {imageLoaded ? (
-                <img className="card-to-guess" src={imageUrl} alt={altText} />
+                <div className="card-image-container">
+                    <img className="card-to-guess" src={imageUrl} alt={altText} />
+                </div>
             ) : (
-                <img className="card-to-guess" width="375px" height="517px" src="/images/cards/loading.webp" alt="Loading Image" />
+                <div className="card-image-container">
+                    <img className="card-to-guess" width="375px" height="517px" src="/images/cards/loading.webp" alt="Loading Image" />
+                </div>
             )}
         </div>
     );
